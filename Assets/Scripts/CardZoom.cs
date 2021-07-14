c8897661f8605374d0a32df7ff95f2b87e6958ee
Arui <41237117+AruiGit@users.noreply.github.com>
2021-07-14 23:28:30 +0200

    Initial commit

---
+++ Assets/Scripts/CardZoom.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardZoom : MonoBehaviour
+{
+    public GameObject Canvas;
+    private GameObject ZoomCard;
+    
+    
+
+    private void Awake()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+    }
+
+    public void OnHoverEnter()
+    {
+        ZoomCard = Instantiate(gameObject, new Vector2(400, 225), Quaternion.identity);
+      
+        ZoomCard.GetComponent<Image>().raycastTarget = false;
+        ZoomCard.name = "ZoomedCard";
+        ZoomCard.transform.SetParent(Canvas.transform, false);
+        ZoomCard.layer = LayerMask.NameToLayer("Zoom");
+
+        RectTransform rect = ZoomCard.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(120, 160);
+    }
+
+    public void OnHoverExit()
+    {
+        Destroy(ZoomCard);
+    }
+
+    public void OnDragging()
+    {
+        Destroy(ZoomCard);
+    }
+}
---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DragAndDrop : NetworkBehaviour
+{
+    public GameObject Canvas;
+    public GameObject Dropzone;
+    public PlayerManager PlayerManager;
+
+    private bool isDragging = false;
+    private bool isDraggable = true;
+
+    private Vector2 startPosition;
+    private GameObject startParent;
+    private bool isOverDropZone = false;
+    private GameObject DropZone;
+    Vector2 mousePosition;
+
+    private void Start()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("DropZone");
+        if (!hasAuthority)
+        {
+            isDraggable = false;
+        }
+    }
+
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition)
+        if (isDragging)
+        {
+            transform.position = new Vector2(mousePosition.x, mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "DropZone")
+        {
+            isOverDropZone = true;
+            DropZone = collision.gameObject;
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+    }
+
+    public void StartDrag()
+    {
+        if (!isDraggable) return;
+        startPosition = transform.position;
+        isDragging = true;
+        startParent = transform.parent.gameObject;
+    }
+
+    public void EndDrag()
+    {
+        if (!isDraggable) return;
+        isDragging = false;
+        
+
+        if (isOverDropZone)
+        {
+            transform.SetParent(DropZone.transform, false);
+            isDraggable = false;
+            NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+            PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+            PlayerManager.PlayCard(gameObject);
+        }
+        else {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
+    }
+
+    
+}
